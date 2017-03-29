302e798e0dd66979a0b6dfee817dd302f1ab48ff
Michal Sládek <slademi2@fit.cvut.cz>
2017-03-29 19:19:41 +0200

    second

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2017.02.25*)
+
+Paclet[
+  Name -> "GoProController",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
