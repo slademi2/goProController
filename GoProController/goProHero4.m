3eba0d99a30217551c04ed79f947284c3fc03f04
Michal Sládek <slademi2@fit.cvut.cz>
2017-04-06 00:20:47 +0200

    Further options for HERO4

--- GoProController/goProHero4.m
+++ GoProController/goProHero4.m
@@ -174,15 +174,230 @@ goProSwitchLowLightOff::usage =
 (* ::Subsection:: *)
 (* Spot Meter *)
 
-goProGetPossibleSpotMeter::usage =
-    "goProGetPossibleSpotMeter[ ] returns possible parameters for SpotMeter settings"
-goProSetSpotMeter::usage =
-    "goProSetSpotMeter[ _String] sets SpotMeter to parameter given. To see which parameters are supported call goProGetPossibleSpotMeter[]."
-goProSwitchSpotMeterOn::usage =
-    "goProSwitchSpotMeterOn[ ] turns SpotMeter on."
-goProSwitchSpotMeterOff::usage =
-    "goProSwitchSpotMeterOff[ ] turns SpotMeter off."
+goProGetPossibleVideoSpotMeter::usage =
+    "goProGetPossibleVideoSpotMeter[ ] returns possible parameters for SpotMeter settings"
+goProSetVideoSpotMeter::usage =
+    "goProSetVideoSpotMeter[ _String] sets SpotMeter to parameter given. To see which parameters are supported call goProGetPossibleVideoSpotMeter[]."
+goProSwitchVideoSpotMeterOn::usage =
+    "goProSwitchVideoSpotMeterOn[ ] turns SpotMeter on."
+goProSwitchVideoSpotMeterOff::usage =
+    "goProSwitchVideoSpotMeterOff[ ] turns SpotMeter off."
 
+
+(* ::Subsection:: *)
+(* Video Loop *)
+goProGetPossibleVideoLoop::usage =
+    "goProGetPossibleVideoLoop[ ] returns possible values of Video Loop"
+goProSetVideoLoop::usage =
+    "goProSetVideoLoop[_String] Sets Video Loop to parameter given.
+	 To see which parameters you can use for Video Loop use function goProGetPossibleVideoLoop[ ]"
+
+
+
+(* ::Subsection:: *)
+(* Photo In video *)
+goProGetPossiblePhotoInVideo::usage =
+    "goProGetPossiblePhotoInVideo[ ] returns possible vaules of time in which will camera take photo during video recording."
+goProSetPhotoInVideo::usage =
+    "goProSetPhotoInVideo[ _String] Sets time interval in which will camera take photo during video recording."  
+
+
+
+
+(* ::Subsection:: *)
+(* Photo Resolution *)
+
+goProSetPhotoRes::usage =
+    "goProSetPhotoRes[_String] Sets Video resolution acording to parameter. To see which parameter you can be used use function goProGetPossiblePhotoRes[]"
+goProGetPossiblePhotoRes::usage =
+    "goProGetPossiblePhotoRes[ ] Returns list of possible parameters for function goProSetVideoRes[_String]"
+
+
+
+
+(* ::Subsection:: *)
+(* NightPhoto time exposure *)
+goProGetPossibleNightPhotoExposureTime::usage=
+	"goProGetPossibleNightPhotoExposureTime[] returns possible values for Night Photo Exposure time."
+goProSetNightPhotoExposureTime::usage=
+	"goProSetNightPhotoExposureTime[ _String] sets Exposure Time for Night Photo to parameter given, 
+	to see which parameter you can use call goProGetPossibleNightPhotoExposureTime[] function."
+
+
+
+(* ::Subsection:: *)
+(* Spot Meter *)
+
+goProGetPossiblePhotoSpotMeter::usage =
+    "goProGetPossiblePhotoSpotMeter[ ] returns possible parameters for SpotMeter settings"
+goProSetPhotoSpotMeter::usage =
+    "goProSetPhotoSpotMeter[ _String] sets SpotMeter to parameter given. To see which parameters are supported call goProGetPossiblePhotoSpotMeter[]."
+goProSwitchPhotoSpotMeterOn::usage =
+    "goProSwitchPhotoSpotMeterOn[ ] turns SpotMeter on."
+goProSwitchPhotoSpotMeterOff::usage =
+    "goProSwitchPhotoSpotMeterOff[ ] turns SpotMeter off."
+
+
+
+(* ::Subsection:: *)
+(* Continuous Shot *)    
+goProGetPossibleContinuousShot::usage =
+    "goProGetContinuousShot[ ] returns possible values of Continuous Shot."
+goProSetContinuousShot::usage =
+    "goProSetContinuousShot[ _String] Sets Continuous Shot to parameter given."
+
+
+(* ::Subsection:: *)
+(* Exposure Time for NightLapse *)
+goProGetPossibleNightLapseExposureTime::usage=
+	"goProGetPossibleNightLapseExposureTime[] returns possible values for NightLapse Exposure time."
+goProSetNightLapseExposureTime::usage=
+	"goProSetNightLapseExposureTime[ _String] sets NightLapse exposure time to value given.
+	to see which values you can use call goProGetPossibleNightLapseExposureTime[]. "
+
+
+
+(* ::Subsection:: *)
+(* Night Lapse interval *)
+goProGetPossibleNightLapseInterval::usage=
+	"goProGetPossibleNightLapseInterval[] returns value usable for night lapse time interval."
+goProSetNightLapseInterval::usage=
+	"goProSetNightLapseInterval[ _String] sets time interval of night lapse to parameter given."
+
+
+
+
+(* ::Subsection:: *)
+(* MultiShot resolution *)
+goProSetMultiShotResolution::usage=
+	"goProSetMultiShotResolution[ _String] sets resolution of multi shot to parameter given."
+goProGetPossibleMultiShotResolution::usage=
+	"goProGetPossibleMultiShotResolution[ ] returns values usable for multi shot resolution."
+
+
+
+(* ::Subsection:: *)
+(* TimeLapse interval *)
+goProSetTimeLapseInterval::usage=
+	"goProSetTimeLapseInterval[ _String] set TimeLapse interval to parameter given."
+goProGetPossibleTimeLapseInterval::usage=
+	"goProGetPossibleTimeLapseInterval[ ] returns usable values for TimeLapse interval."
+	
+	
+	
+
+(* ::Subsection:: *)
+(* MultiShot spot meter *)
+goProGetPossibleMultiShotSpotMeter::usage =
+    "goProGetPossibleMultiShotSpotMeter[ ] returns possible parameters for SpotMeter settings"
+goProSetMultiShotSpotMeter::usage =
+    "goProSetMultiShotSpotMeter[ _String] sets SpotMeter to parameter given. To see which parameters are supported call goProGetPossibleMultiShotSpotMeter[]."
+goProSwitchMultiShotSpotMeterOn::usage =
+    "goProSwitchMultiShotSpotMeterOn[ ] turns SpotMeter on."
+goProSwitchMultiShotSpotMeterOff::usage =
+    "goProSwitchMultiShotSpotMeterOff[ ] turns SpotMeter off."
+	
+	
+
+
+(* ::Subsection:: *)
+(* BurstRate *)
+goProGetPossibleBurstRate::usage =
+    "goProGetPossibleBurstRate[ ] returns possible values of BurstRate"
+goProSetBurstRate::usage =
+    "goProSetBurstRate[_String] Sets BurstRate to parameter given.
+	 To see which parameters you can use for BurstRate use function goProGetPossibleBurstRate[ ]"
+	
+	
+
+
+
+(* ::Subsection:: *)
+(* Orientation *)
+(*Orientation*)
+goProSetOrientation::usage =
+    "goProSetOrientation[ _String] sets orientation of your display based on parameter."
+goProGetPossibleOrientation::usage =
+    "goPoGetPossibleOrientation[ ] returns possible parameters for orientation of display."
+    
+    
+
+(* ::Subsection:: *)
+(* QuickCature *)
+goProGetPossibleQuickCapture::usage=
+	"goProGetPossibleQuickCapture[ ] returns values usable for quick capture."
+goProSetQuickCapture::usage=
+	"goProSetQuickCapture[ _String] sets quick capture to parameter given."
+goProSwitchQuickCaptureOn::usage=
+	"goProSwitchQuickCaptureOn[ ] switches quick capture on."
+goProSwitchQuickCaptureOff::usage=
+	"goProSwitchQuickCaptureOff[ ] switches quick capture off."
+
+
+
+(* ::Subsection:: *)
+(* LED *)
+goProGetPossibleLed::usage =
+    "goProGetPossibleLed[ ] returns possible parameters for Led settings"
+goProSetLed::usage =
+    "goProSetLed[ _String] sets leds to parameter given. To see which parameter you can use call goProGetPossibleLed[]."
+
+(* ::Subsection:: *)
+(* Volume *)
+goProGetPossibleVolume::usage =
+    "goProGetPossibleVolume[ ] returns possible values for volume settings."
+goProSetVolume::usage =
+    "goProSetVolume[ _String] sets volume to parameter given. For values which are usable see goProGetPossibleVolume[]."
+ 
+
+
+(* ::Subsection:: *)
+(* Video Mode *)
+goProGetPossibleVideoMode::usage =
+    "goProGetPossibleVideoMode[ ] returns possible parameters for VideoMode settings"
+goProSetVideoMode::usage =
+    "goProSetVideoMode[ _String] sets leds to parameter given. To see which parameter you can use call goProGetPossibleVideoMode[]."
+
+
+
+
+(* ::Subsection:: *)
+(* LCD Display *)
+goProSwitchLCDDisplayOn::usage=
+	"goProSetLCDDisplayOn[ ] switches LCD display on" 
+goProSwitchLCDDisplayOff::usage=
+	"goProSetLCDDisplayOff[ ] switches LCD display off"
+
+
+
+(* ::Subsection:: *)
+(* AutoPowerOff *)
+(*Auto Power Off*)
+goProGetPossibleAutoPowerOff::usage =
+    "goProGetPossibleAutoPowerOff[ ] returns possible parameters in minutes for AutoPowerOff settings"
+goProSetAutoPowerOff::usage =
+    "goProSetAutoPowerOff[ _String] sets AutoPowerOff to parameter given. To see which parameters are supported call goProGetPossibleAutoPowerOff []."
+
+
+
+
+(* ::Subsection:: *)
+(* Locate *)
+goProLocate::usage =
+    "goProLocate[ ] camera will start sound signal to locate your camera."
+goProLocateStop::usage =
+    "goProLocateStop[ ] camera will stop sound signal to locate your camera."
+
+
+
+(* ::Subsection:: *)
+(* Delete *)
+goProDeleteFile::usage=
+	"goProDeleteFile[ _String] deletes file which name was given as parameter. Insert name in format /100GOPRO/file"
+goProDeleteLast::usage=
+	"goProDeleteLast[ ] deletes last file from memory."
+goProDeleteAll::usage=
+	"goProDeleteAll[ ] deletes all files from memory."
 
 
 
@@ -492,25 +707,336 @@ goProSwitchLowLightOff[]:=goProSetLowLight["off"]
 
 (* ::Subsection:: *)
 (* Spot Meter *)
-spotMeter="9"
+videoSpotMeter="9"
 spotMeterToCode=<|"on"->"1","off"->"0"|>
 spotMeterPossible={"on","off"}
-goProSetSpotMeter[param_String]:=execute[goProMakeCommand["setting",spotMeter,spotMeterToCode[[param]]]]
-goProGetPossibleSpotMeter[]:=spotMeterPossible
-goProSwitchSpotMeterOn[]:=goProSetSpotMeter["on"]
-goProSwitchSpotMeterOff[]:=goProSetSpotMeter["off"]
+goProSetVideoSpotMeter[param_String]:=execute[goProMakeCommand["setting",videoSpotMeter,spotMeterToCode[[param]]]]
+goProGetPossibleVideoSpotMeter[]:=spotMeterPossible
+goProSwitchVideoSpotMeterOn[]:=goProSetVideoSpotMeter["on"]
+goProSwitchVideoSpotMeterOff[]:=goProSetVideoSpotMeter["off"]
 
 
 
 
+
+(* ::Subsection:: *)
+(* Video Loop *)
+videoLoop="6"
+videoLoopToCode = <|
+   "5"->"1",
+   "20"->"2",
+   "60"->"3",
+   "120"->"4",
+   "max"->"0"
+   |>;
+videoLoopPossible = {"max","5","20","60","120"}
+goProGetPossibleVideoLoop[] :=videoLoopPossible
+goProSetVideoLoop[param_String]:=execute[goProMakeCommand["setting",videoLoop,videoLoopToCode[[param]]]]
+
+
+
+(* ::Subsection:: *)
+(* PhotonVideo *)
+photoInVideo="7"
+photoInVideoToCode = <|
+    "5"->"1",
+    "10"->"2",
+    "30"->"3",
+    "60"->"4"
+|>
+
+photoInVideoPossible = {"5","10","30","60"}
+goProGetPossiblePhotoInVideo[] :=photoInVideoPossible
+goProSetPhotoInVideo[param_String] :=execute[goProMakeCommand["setting",photoInVideo,photoInVideoToCode[[param]]]]
+
+
+
+
+
+(* ::Subsection:: *)
+(* Photo Resolution *)
+
+photoRes="17"
+photoResToCode = <|
+   "5M" -> "3",
+   "7W" -> "1",
+   "12W" -> "0",
+   "7M" -> "2"
+   |>;
+     
+photoResPossible = {
+   "12W",
+   "7W",
+   "7M",
+   "5M"
+};
+
+ 
+goProSetPhotoRes[param_String] := execute[goProMakeCommand["setting",photoRes,photoResToCode[[param]]]]
+goProGetPossiblePhotoRes[] :=photoResPossible
+
+
+
+(* ::Subsection:: *)
+(* NightPhotoExposureTime *)
+nightPhotoExTime="19"
+nightPhotoExTimeToCode=<|"auto"->"0",
+"2"->"1",
+"5"->"2",
+"10"->"3",
+"15"->"4",
+"20"->"5",
+"30"->"6"
+|>
+nightPhotoExTimePossible={"auto","2","5","10","15","20","30"}
+goProGetPossibleNightPhotoExposureTime[]:=nightPhotoExTimePossible
+goProSetNightPhotoExposureTime[param_String]:=execute[goProMakeCommand["setting",nightPhotoExTime,nightPhotoExTimeToCode[[param]]]]
+
+
+(* ::Subsection:: *)
+(* Photo Spot Meter *)
+photoSpotMeter="20"
+goProSetPhotoSpotMeter[param_String]:=execute[goProMakeCommand["setting",photoSpotMeter,spotMeterToCode[[param]]]]
+goProGetPossiblePhotoSpotMeter[]:=spotMeterPossible
+goProSwitchPhotoSpotMeterOn[]:=goProSetPhotoSpotMeter["on"]
+goProSwitchPhotoSpotMeterOff[]:=goProSetPhotoSpotMeter["off"]
+
+
+
+
+(* ::Subsection:: *)
+(* ContinuousPhoto *)
+continuousShot="18"
+continuousShotToCode = <|
+   "3"->"0",
+   "5"->"1",
+   "10"->"2"
+   |>;
+continuousShotPossible = {"3","5","10"}
+goProGetPossibleContinuousShot[] :=continuousShotPossible
+goProSetContinuousShot[param_String] :=execute[goProMakeCommand["setting",continuousShot,continuousShotToCode[[param]]]]
+
+
+
+
+(* ::Subsection:: *)
+(* NightLapseExposureTime *)
+
+nightLapseExTime="31"
+nightLapseExTimeToCode=<|"auto"->"0",
+"2"->"1",
+"5"->"2",
+"10"->"3",
+"15"->"4",
+"20"->"5",
+"30"->"6"
+|>
+nightLapseExTimePossible={"auto","2","5","10","15","20","30"}
+goProGetPossibleNightLapseExposureTime[]:=nightLapseExTimePossible
+goProSetNightLapseExposureTime[param_String]:=execute[goProMakeCommand["setting",nightLapseExTime,nightLapseExTimeToCode[[param]]]]
+
+	
+	
+
+(* ::Subsection:: *)
+(* NightLapseTimeInterval *)
+nightLapseInterval="32"
+nightLapseIntervalPossible={"continuous","4s","5s","10s","15s","20s","30s","1m","2m","5m","30m","60m"}
+nightLapseIntervalToCode=<|
+"continuous"->"0",
+"4s"->"4",
+"5s"->"5",
+"10s"->"10",
+"15s"->"15",
+"20s"->"20",
+"30s"->"30",
+"1m"->"60",
+"2m"->"120",
+"5m"->"300",
+"30m"->"1800",
+"60m"->"3600"
+|>
+goProGetPossibleNightLapseInterval[]:=nightLapseIntervalPossible
+goProSetNightLapseInterval[param_String]:=execute[goProMakeCommand["setting",nightLapseInterval,nightLapseIntervalToCode[[param]]]]
+	
+	
+	
+	
+
+(* ::Subsection:: *)
+(* MultiShotresolution *)
+multiShotResolution="28"
+goProSetMultiShotResolution[param_String]:=execute[goProMakeCommand["setting",multiShotResolution,photoResToCode[[param]]]]
+goProGetPossibleMultiShotResolution[]:=photoResPossible
+	
+
+
+(* ::Subsection:: *)
+(* TimeLapse Interval *)
+timeLapseInterval="30"
+timeLapseIntervalToCode=<|"0.5"->"0",
+"1"->"1",
+"2"->"2",
+"5"->"5",
+"10"->"10",
+"30"->"30",
+"60"->"60"
+|>
+timeLapseIntervalPossible={"0.5","1","2","5","10","30","60"}
+goProSetTimeLapseInterval[param_String]:=execute[goProMakeCommand["setting",timeLapseInterval,timeLapseIntervalToCode[[param]]]]
+goProGetPossibleTimeLapseInterval[]:=timeLapseIntervalPossible
+
+(* ::Subsection:: *)
+(* Photo Spot Meter *)
+multiShotSpotMeter="33"
+goProSetMultiShotSpotMeter[param_String]:=execute[goProMakeCommand["setting",multiShotSpotMeter,spotMeterToCode[[param]]]]
+goProGetPossibleMultiShotSpotMeter[]:=spotMeterPossible
+goProSwitchMultiShotSpotMeterOn[]:=goProSetMultiShotSpotMeter["on"]
+goProSwitchMultiShotSpotMeterOff[]:=goProSetMultiShotSpotMeter["off"]
+
+
+
+
+(* ::Subsection:: *)
+(* Burst Rate *)
+burstRate="29"
+burstRateToCode = <|
+   "3/1"->"0",
+   "5/1"->"1",
+   "10/1"->"2",
+   "10/2"->"3",
+   "10/3"->"4",
+   "30/1"->"5",
+   "30/2"->"6",
+   "30/3"->"7",
+   "30/6"->"8"
+   |>;
+burstRatePossible = {"3/1","5/1","10/1","10/2","10/3","30/1","30/2","30/3","30/6"}
+
+goProGetPossibleBurstRate[] := burstRatePossible
+goProSetBurstRate[param_String] := execute[goProMakeCommand["setting",burstRate,burstRateToCode[[param]]]]
+
+
+
+
+(* ::Subsection:: *)
+(* Orientation *)
+orientation="52"
+orientationPossible = {"up","down","gyro"}
+orientationToCode=<|"up"->"1","down"->"2","gyro"->"0"|>
+goProSetOrientation[param_String]:=execute[goProMakeCommand["setting",orientation,orientationToCode[[param]]]]
+goProGetPossibleOrientation[]:=orientationPossible
+
+
+
+(* ::Subsection:: *)
+(* Quick Capture *)
+quickCapture="54"
+goProGetPossibleQuickCapture[]:=spotMeterPossible
+goProSetQuickCapture[param_String]:=execute[goProMakeCommand["setting",quickCapture,spotMeterToCode[[param]]]]
+goProSwitchQuickCaptureOn[]:=goProSetQuickCapture["on"]
+goProSwitchQuickCaptureOff[]:=goProSetQuickCapture["off"]
+
+
+
+(* ::Subsection:: *)
+(* LED *)
+led="55"
+ledToCode = <|
+    "off"->"0",
+    "2"->"1",
+    "4"->"2"
+|>
+ledPossible = {"off","2","4"}
+goProGetPossibleLed[] := ledPossible
+goProSetLed[param_String] := execute[goProMakeCommand["setting",led,ledToCode[[param]]]]
+
+
+
+(* ::Subsection:: *)
+(* Volume *)
+volume="56"
+volumeToCode = <|
+    "off"->"2",
+    "70"->"1",
+    "100"->"0"
+|>
+volumePossible = {"off","70","100"}
+goProGetPossibleVolume[] :=volumePossible
+goProSetVolume[param_String] := execute[goProMakeCommand["setting",volume,volumeToCode[[param]]]]
+ 
+
+
+
+
+(* ::Subsection:: *)
+(* VideoMode *)
+videoMode="57"
+videoModeToCode = <|
+    "ntsc"->"0",
+    "pal"->"1"
+|>
+videoModePossible = {"ntsc","pal"}
+goProGetPossibleVideoMode[] := videoModePossible
+goProSetVideoMode[param_String] :=execute[goProMakeCommand["setting",videoMode,videoModeToCode[[param]]]]
+
+
+(* ::Subsection:: *)
+(* LCD Display *)
+lcdDisplay="72"
+goProSwitchLCDDisplayOn[]:=execute[goProMakeCommand["setting",lcdDisplay,"1"]]
+goProSwitchLCDDisplayOff[]:=execute[goProMakeCommand["setting",lcdDisplay,"0"]]
+
+
+
+(* ::Subsection:: *)
+(* LCD display Brightness *)
+(*lcdBrightness="49"
+lcdBrightnessToCode=<|"high"->"0","medium"->"1","low"->"2"|>
+lcdBrightnessPossible={"high","medium","low"}
+goProSetLCDBrightness[param_String]:=execute[goProMakeCommand["setting",lcdBrightness,lcdBrightnessToCode[[param]]]]
+goProGetPossibleLCDBrightness[]:=lcdBrightnessPossible*)
+
+
+
+(* ::Subsection:: *)
+(* Auto off	 *)
+autoOff="59"
+autoOffToCode=<|"never"->"0",
+"1"->"1",
+"2"->"2",
+"3"->"3",
+"5"->"4"|>
+autoOffPossible={"never","1","2","3","5"}
+goProSetAutoPowerOff[param_String]:=execute[goProMakeCommand["setting",autoOff,autoOffToCode[[param]]]]
+goProGetPossibleAutoPowerOff[]:=autoOffPossible
+
+
+
+(* ::Subsection:: *)
+(* Locate *)
+
+goProLocate[] :=execute[goProMakeCommand["command/system","locate?p=1"]]
+goProLocateStop[] :=execute[goProMakeCommand["command/system","locate?p=0"]]
+
+
+
+(* ::Subsection:: *)
+(* Delete *)
+goProDeleteFile[param_String]:=execute[goProMakeCommand["command/storage","delete?p="<>param]]
+goProDeleteLast[]:=execute[goProMakeCommand["command/storage/delete","last"]]
+goProDeleteAll[]:=execute[goProMakeCommand["command/storage/delete","all"]]
+
+
+
+
+	
 	
 (* ::Subsection:: *)
 (* VIDEO Shutter Speed *)
 shutterSpeedToCode=<|
-	
 |>
 shutterSpeedPossible={}
-
 (*goProPossibleVideoShutterSpeed[]:=*)
 
 
@@ -520,5 +1046,6 @@ shutterSpeedPossible={}
 (* Region Title *)
 
 End[]
+
 
 EndPackage[]
